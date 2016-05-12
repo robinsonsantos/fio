7b73ac2afa03c8667e9d2006956564728855800a
Robinson D. S. Santos <stanleymagrao@gmail.com>
2016-05-12 08:41:37 -0300

    Slider was removed

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on available packages.
-
 from gevent import monkey
 monkey.patch_all()
 
@@ -18,9 +14,14 @@ socketio = SocketIO(app, async_mode='gevent')
 def index():
     return render_template('index.html')
 
-@socketio.on('my broadcast event', namespace='/test')
+@socketio.on('broadcast event', namespace='/test')
 def test_broadcast_message(message):
     emit('my response', {'data': message['data']}, broadcast=True)
 
+@socketio.on('slider event', namespace='/test')
+def test_slider_message(message):
+    print 'message', message
+    #emit('my response', {'data': message['data']}, broadcast=True)
+    
 if __name__ == '__main__':
     socketio.run(app, debug=True)
