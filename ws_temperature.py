42cb1095c013c41b2f9db56a81d3ea785ca2f5b2
Robinson D. S. Santos <stanleymagrao@gmail.com>
2016-05-13 15:57:08 -0300

    Refactored

--- ws_temperature.py
+++ ws_temperature.py
@@ -22,5 +22,5 @@ def test_broadcast_message(message):
     emit('temperature update', {'data': message['data']}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
